c558e9bc966bc5f011d77ed6319b23b8cda198be
Seanlau <lausean561@gmail.com>
2023-02-13 23:16:14 +0800

    设置导航栏

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,14 @@
 module.exports={
+    title:'Mooc-UI',
     base:'/louis-moocUI/',
     themeConfig:{
+        nav:[{
+            text:'Home',link:'/'
+        },{
+            text:'Github',link:'https://github.com/Seanlau01/louis-moocUI'
+        },{
+            text:'Npm',link:'https://www.npmjs.com/package/louis-mooc-ui'
+        }],
         sidebar:[
             '/',
             '/componentDocs/card'
